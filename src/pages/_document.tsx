2c94ffc8964140898fe982d1aa3d94afad9207a9
Juanita Afanador <106590201+JuanitaAK@users.noreply.github.com>
2023-09-05 15:53:23 +0200

    adding error-about pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <Main />
         <NextScript />
       </body>
