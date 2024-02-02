4adff07c67140421c092dbed0fcd521848f2adeb
Juanita Afanador <106590201+JuanitaAK@users.noreply.github.com>
2024-02-02 23:09:03 +0100

    chore: title padding

--- src/components/storiesCard/StoryCard.tsx
+++ src/components/storiesCard/StoryCard.tsx
@@ -6,7 +6,7 @@ export const StoryCard = (props: Story): JSX.Element => {
       key={props.id_story}
       className="block rounded-lg bg-story shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] m-5 mb-8 p-5"
     >
-      <h5 className="my-6 py-3 text-2xl text-center font-extrabold leading-tight text-blue-800">
+      <h5 className="my-6 pb-3 text-2xl text-center font-extrabold leading-tight text-blue-800">
         {props.title?.toUpperCase()}
       </h5>
       <p className="mb-4 text-base text-blue-800">{props.story}</p>
