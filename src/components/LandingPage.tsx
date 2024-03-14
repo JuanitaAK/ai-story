f2b869fdff545e4678d74c4a63372f886d53f1d3
Juanita Afanador <106590201+JuanitaAK@users.noreply.github.com>
2024-03-14 01:17:39 +0100

    chore: refactor LandingPage and Footer components mobile first adjustments

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -9,32 +9,32 @@ const LandingPage = (): JSX.Element => {
   return (
     <div>
       {/* // <div className="bg-gradient-to-b from-white to-blue-500"> */}
-      <h2 className=" text-center text-3xl lg:text-5xl font-bold text-nav-font m-6 p-3">
+      <h2 className=" text-center text-3xl lg:text-5xl font-bold text-nav-font mb-6">
         MyStory.com
       </h2>
 
-      <div className="flex flex-col lg:flex-row items-start">
+      <div className="flex flex-col lg:flex-row gap-6 items-start">
         <Image
           src={home}
           alt="Family reading a book"
           className=" rounded-lg left-6"
         />
 
-        <div className="Card-container m-3 bg-white p-6 rounded-lg shadow-lg bg-opacity-70  max-w-lg  ">
-          <p className="text-lg font-medium text-neutral-700">
+        <div className="Card-container m-3 lg:mt-12 bg-white p-6 rounded-lg shadow-lg bg-opacity-70  max-w-lg  ">
+          <p className="text-lg lg:text-2xl text-neutral-700">
             MyStory is a platform where you can create explore your creativity
             to create your own stories in a simple way and share it.
           </p>
           <div>
-            <div className="buttons_container flex flex-col lg:flex-row gap-5 m-5 ">
+            <div className="buttons_container flex items-center flex-col lg:flex-row gap-5 m-5 py-5">
               <Link href="/login">
-                <button className="self-end w-full sm:w-48 p-2  text-white rounded-md bg-button hover:bg-hover transition duration-300">
+                <button className="self-end w-full lg:w-48 py-2 px-10 lg:text-2xl text-white rounded-md bg-button hover:bg-hover transition duration-300">
                   Login
                 </button>
               </Link>
 
               <Link href="/signup">
-                <button className="self-end w-full sm:w-48 p-2 text-white rounded-md bg-button hover:bg-hover transition duration-300">
+                <button className="self-end w-full lg:w-48  py-2 px-7 lg:text-2xl  text-white rounded-md bg-button hover:bg-hover transition duration-300">
                   Register
                 </button>
               </Link>
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 const Footer = (): JSX.Element => {
   return (
-    <div className="flex justify-end items-start px-32 py-3 bg-bgFooter shadow-lg text-gray-200">
+    <div className="flex sm:justify-end justify-center py-3 px-3 bg-bgFooter shadow-lg text-gray-200 lg:px-24 xl:px-32">
       <Link
         href="/about"
-        className="hover:bg-hover hover:text-white rounded-md px-3 py-2 text-base sm:text-xl font-medium mb-2 sm:mb-0 mr-4"
+        className="hover:bg-hover hover:text-white rounded-md py-2 text-base sm:text-xl font-medium mb-2 sm:mb-0 mr-4"
       >
         About Story.com
       </Link>
       <Link
         href="/contact"
-        className="hover:bg-hover hover:text-white rounded-md px-3 py-2 text-base sm:text-xl font-medium"
+        className="hover:bg-hover hover:text-white rounded-md  py-2 text-base sm:text-xl font-medium mb-2 sm:mb-0 mr-4"
       >
         Contact
       </Link>
