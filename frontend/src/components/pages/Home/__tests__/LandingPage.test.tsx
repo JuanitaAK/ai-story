a513cc4a13bf3651bf1e31fbf49de80acd4c24ab
Juanita Afanador <106590201+JuanitaAK@users.noreply.github.com>
2024-04-30 15:35:09 +0200

    chore: correcting test Landing page

--- frontend/src/components/pages/Home/__tests__/LandingPage.test.tsx
+++ frontend/src/components/pages/Home/__tests__/LandingPage.test.tsx
@@ -3,7 +3,7 @@ import "@testing-library/jest-dom";
 import LandingPage from "../LandingPage";
 
 describe("LandingPage", () => {
-  it("should render a landing page with an image", () => {
+  it("should render a landing page with an image that has an alt description", () => {
     render(<LandingPage />);
 
     const mainImage = screen.getByAltText("Family reading a book");
@@ -11,15 +11,15 @@ describe("LandingPage", () => {
     expect(mainImage).toBeInTheDocument();
   });
 
-  it("should render a button with the text 'Login'", () => {
+  it("should render a button with the text 'Sign in'", () => {
     render(<LandingPage />);
-    const loginButton = screen.getByText("Login");
+    const loginButton = screen.getByRole("link", { name: "Sign in" });
     expect(loginButton).toBeVisible();
   });
 
-  it("should render a button with the text 'Register'", () => {
+  it("should render a button with the text 'Sign Up'", () => {
     render(<LandingPage />);
-    const registerButton = screen.getByText("Register");
+    const registerButton = screen.getByRole("link", { name: "Sign up" });
     expect(registerButton).toBeVisible();
   });
 });
