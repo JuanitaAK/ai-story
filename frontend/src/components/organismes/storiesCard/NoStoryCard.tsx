dd1c105bf4fe911113acc20bd0abfe918049e3bb
Juanita Afanador <106590201+JuanitaAK@users.noreply.github.com>
2024-04-30 09:48:36 +0200

    feat: add link to create a new story in NoStoryCard component

--- frontend/src/components/organismes/storiesCard/NoStoryCard.tsx
+++ frontend/src/components/organismes/storiesCard/NoStoryCard.tsx
@@ -1,15 +1,26 @@
+import Link from "next/link";
+
 export const NoStoryCard = (): JSX.Element => {
   return (
     <div className="block rounded-lg md:max-w-3xl max-w-2xl bg-story shadow-xl m-12 mb-8 p-7">
       <p className="my-6 pb-3 text-3xl md:text-4xl text-center font-extrabold leading-tight text-title">
         You have no stories at the moment 🙈
       </p>
-      <p className="my-6 pb-3 text-3xl md:text-4xl text-center font-extrabold leading-tight text-title">
+      <p className="my-6 pb-3 text-2xl md:text-4xl text-center font-extrabold leading-tight text-title">
         Go and create one! 😎
       </p>
-      <p className="text-center  md:text-3xl text-title">
-        Click on the button below to create your first story! 🚀
+      <p className="text-center md:text-3xl text-title">
+        Click on the button below to create your first story!
       </p>
+
+      <div className="mt-6 flex justify-end items-end">
+        <Link
+          href="/form"
+          className="flex items-center text-lg justify-center py-3 px-5 w-48 mt-4 text-white rounded-md  bg-button hover:bg-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-hover transition duration-300"
+        >
+          New Story 🚀
+        </Link>
+      </div>
     </div>
   );
 };
